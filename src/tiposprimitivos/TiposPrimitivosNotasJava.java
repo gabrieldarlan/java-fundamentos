f1500d10b0e55f3b48a71c2f346ba43ee206629d
Gabriel Darlan <gabriel.darlan88@gmail.com>
2024-09-28 18:14:44 -0300

    aula completa

--- src/tiposprimitivos/TiposPrimitivosNotasJava.java
+++ src/tiposprimitivos/TiposPrimitivosNotasJava.java
@@ -6,6 +6,25 @@ package tiposprimitivos;
 public class TiposPrimitivosNotasJava {
     public static void main(String[] args) {
         // Caracteres ou Characters
+        char primeiraLetraDoNome='G';
+        char primeiraLetraDoNomeASCII = 71;
+
+        System.out.println(primeiraLetraDoNome);
+        System.out.println(primeiraLetraDoNomeASCII);
+
+        // Inteiros ou Integers
+        byte nota = 10; // -128 até 127
+        short itensComprados = 2; // -32768 até 32767
+        int quantidadeEstoque = 23_366; // -2_147_483_648 até 2_147_483_647
+        long numeroMuitoGrande = 7809709870987098709L; // 19 dígitos
+
+        // Tipos Decimais
+        float peso = -1.5f; // 6 - 7 casas decimais
+        double preco = 24.99; // ~ 15 casas decimais
+
+        // Booleanos ou Booleans
+        boolean verdadeiro = true;
+        boolean falso = false;
 
     }
 }
